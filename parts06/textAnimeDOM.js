068f89953a6a30e9009f9ee19f48fd410dbf4c42
Chimoha31 <mokochii1108@gmail.com>
2021-12-28 00:15:27 -0800

    created intersection observer files

--- parts06/textAnimeDOM.js
+++ parts06/textAnimeDOM.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const btn = document.querySelector('#btn');
-  const ta = new TextAnimation('.animate-title');
-  const ta2 = new TextAnimation('.animate-title-2');
-  btn.addEventListener('click', () => {
-      ta.animate();
-      ta2.animate();
-    });
+  const btn = document.querySelector("#btn");
+  const ta = new TextAnimation(".animate-title");
+  const ta2 = new TextAnimation(".animate-title-2");
+  btn.addEventListener("click", () => {
+    ta.animate();
+    ta2.animate();
+  });
 });
 
 class TextAnimation {
@@ -21,49 +21,63 @@ class TextAnimation {
     }, "");
   }
   animate() {
-      this.el.classList.toggle('inview');
+    this.el.classList.toggle("inview");
   }
 }
 
+class TextAnimation2 extends TextAnimation {
+  constructor(el) {
+    super(el);
+  }
+  // _splitText() {
+  //   return this.chars.reduce((accu, curr) => {
+  //     curr = curr.replace(" ", "&nbsp;");
+  //     return `${accu}<span class="char">${curr}</span>`;
+  //   }, "");
+  // }
 
-
-// About class
-const obj = {
-  first_name: 'Chiho',
-  last_name: 'Maekawa',
-  printFullName: function() {
-    console.log('hello');
+  // 内容書き換える場合(override)
+  animate() {
+    this.el.classList.toggle("inview");
   }
 }
 
-class MyObj {
-  constructor() {
-    this.first_name = "Teddy";
-    this.last_name= "Sanghera";
-  }
-  printFullName() {
-    console.log('hello2');
-  }
-}
+// // About class
+// const obj = {
+//   first_name: 'Chiho',
+//   last_name: 'Maekawa',
+//   printFullName: function() {
+//     console.log('hello');
+//   }
+// }
 
-const obj2 = new MyObj();
+// class MyObj {
+//   constructor() {
+//     this.first_name = "Teddy";
+//     this.last_name= "Sanghera";
+//   }
+//   printFullName() {
+//     console.log('hello2');
+//   }
+// }
 
-obj.printFullName();
-obj2.printFullName();
+// const obj2 = new MyObj();
 
+// obj.printFullName();
+// obj2.printFullName();
 
-// thisとbind
-const obj3 = {
-  first_name: 'Hana',
-  last_name: 'Maekawa',
-  printFullName: function() {
-    console.log(this);
-    const _that = this;
+// // thisとbind
+// const obj3 = {
+//   first_name: 'Hana',
+//   last_name: 'Maekawa',
+//   printFullName: function() {
+//     console.log(this);
+//     const _that = this;
 
-    window.setTimeout(function() {
-      console.log(this);
-    }.bind(this));
-  }
-}
+//     window.setTimeout(function() {
+//       console.log(this);
+//     }.bind(this));
+//   }
+// }
 
-obj3.printFullName();
+// obj3.printFullName();
