3e4fe5cee7c8a0294b3d4db5ac74c0ee5e42d5f9
Chimoha31 <mokochii1108@gmail.com>
2021-12-30 15:57:47 -0800

    styling for mobile

--- for_mobile_menu/main.js
+++ for_mobile_menu/main.js
@@ -1,27 +1,26 @@
 class MobileMenu {
-  constructor(){
+  constructor() {
     this.DOM = {};
-    this.DOM.btn = document.querySelector('.mobile-menu__btn');
-    this.DOM.container = document.querySelector('#global-container');
+    this.DOM.btn = document.querySelector(".mobile-menu__btn");
+    this.DOM.container = document.querySelector("#global-container");
     this.eventType = this._getEventType();
-    this._addEvent()
+    this._addEvent();
   }
-  
+
   _getEventType() {
-    // 端末がタッチ（タップ）に対応しているか確認する
-    const isTouchCapable = 
-    "ontouchstart" in window || 
-    (window.DocumentTouch && document instanceof window.DocumentTouch) ||
-    navigator.maxTouchPoints > 0 ||
-    window.navigator.msMaxTouchPoints > 0;
+    const isTouchCapable =
+      "ontouchstart" in window ||
+      (window.DocumentTouch && document instanceof window.DocumentTouch) ||
+      navigator.maxTouchPoints > 0 ||
+      window.navigator.msMaxTouchPoints > 0;
 
     return isTouchCapable ? "touchstart" : "click";
   }
-  
+
   _toggle() {
-    this.DOM.container.classList.toggle('menu-open');
+    this.DOM.container.classList.toggle("menu-open");
   }
-  
+
   _addEvent() {
     this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
   }
